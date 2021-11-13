65d18f4b66c026dba5eb8cc46503ac64b7abddf9
wade <wadecroft@gmail.com>
2021-11-13 15:00:07 -0800

    set up basic notification

---
+++ app.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+const WindowsToaster = require("node-notifier").WindowsToaster;
+
+var notifier = new WindowsToaster({
+  withFallback: false, // Fallback to Growl or Balloons?
+  customPath: undefined, // Relative/Absolute path if you want to use your fork of SnoreToast.exe
+});
+
+notifier.notify(
+  {
+    title: "Calendar Notification", // String. Required
+    message: "This is the message", // String. Required if remove is not defined
+    icon: undefined, // String. Absolute path to Icon
+    sound: true, // Bool | String (as defined by http://msdn.microsoft.com/en-us/library/windows/apps/hh761492.aspx)
+    id: 88888, // Number. ID to use for closing notification.
+    appID: undefined, // String. App.ID and app Name. Defaults to no value, causing SnoreToast text to be visible.
+    remove: undefined, // Number. Refer to previously created notification to close.
+    install: undefined, // String (path, application, app id).  Creates a shortcut <path> in the start menu which point to the executable <application>, appID used for the notifications.
+  },
+  function (error, response) {
+    console.log(response);
+  }
+);
